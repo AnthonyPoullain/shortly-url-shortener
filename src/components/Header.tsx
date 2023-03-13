6b0aa09758ee02d4cf7fa7be0e827f59f2f396bf
Anthony <anthonypoullain.pro@gmail.com>
2023-03-13 17:31:39 +0100

    :sparkles: Close mobile menu on click outside & link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,9 @@
 import Logo from '../assets/logo.svg';
-import { useState } from 'react';
+import { useComponentVisible } from '../hooks';
 
 function Header() {
-	const [menuOpen, setMenuOpen] = useState(false);
+	const { ref, isComponentVisible, setIsComponentVisible } =
+		useComponentVisible(false);
 
 	return (
 		<header className="max-w-screen-xl sm:px-10 px-5 py-7 sm:py-10 flex items-center justify-between relative mx-auto">
@@ -54,7 +55,7 @@ function Header() {
 			<button
 				type="button"
 				className="w-5 sm:hidden"
-				onClick={() => setMenuOpen(!menuOpen)}
+				onClick={() => setIsComponentVisible(!isComponentVisible)}
 			>
 				<svg
 					className="text-neutral-grayish-violet fill-current"
@@ -63,18 +64,30 @@ function Header() {
 					<path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
 				</svg>
 			</button>
-			{menuOpen ? (
-				<div className="absolute block sm:hidden top-0 left-1/2 translate-x-[-50%] w-[calc(100vw-40px)] translate-y-20 text-center bg-primary-violet text-white text-sm font-bold p-6 rounded-md">
+			{isComponentVisible && isComponentVisible ? (
+				<div
+					ref={ref}
+					className="absolute block sm:hidden top-0 left-1/2 translate-x-[-50%] w-[calc(100vw-40px)] translate-y-20 text-center bg-primary-violet text-white text-sm font-bold p-6 rounded-md"
+				>
 					<nav>
 						<ul className="flex flex-col gap-5">
 							<li>
-								<a href="#features">Features</a>
+								<a
+									onClick={() => setIsComponentVisible(false)}
+									href="#features"
+								>
+									Features
+								</a>
 							</li>
 							<li>
-								<a href="#">Pricing</a>
+								<a onClick={() => setIsComponentVisible(false)} href="#">
+									Pricing
+								</a>
 							</li>
 							<li>
-								<a href="#">Resources</a>
+								<a onClick={() => setIsComponentVisible(false)} href="#">
+									Resources
+								</a>
 							</li>
 						</ul>
 					</nav>
@@ -82,10 +95,13 @@ function Header() {
 					<nav className="">
 						<ul className="text-sm  text-white flex flex-col gap-5 mb-2 ">
 							<li>
-								<a href="#">Login</a>
+								<a onClick={() => setIsComponentVisible(false)} href="#">
+									Login
+								</a>
 							</li>
 							<li className="">
 								<a
+									onClick={() => setIsComponentVisible(false)}
 									href="#"
 									className="text-white font-bold bg-primary-cyan inline-block w-full py-2 px-4 rounded-3xl hover:opacity-70 whitespace-nowrap"
 								>
